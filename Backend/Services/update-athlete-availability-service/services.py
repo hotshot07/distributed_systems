41b08371772bce011f852e07e4c6df832f271380
Mark Hanrahan <55988775+hanrahma123@users.noreply.github.com>
2022-03-16 21:06:18 +0000

    Upload athlete availability service (#3)
    * First Commit
    
    * changed structure
    
    * edited todos
    
    * removed bad import
    
    * added requirements.txt
    
    * Reworked DB, removed init service
    
    * Created a basic form validator
    
    Co-authored-by: Mark Hanrahan <markh@Marks-MacBook-Pro.local>

---
+++ Backend/Services/update-athlete-availability-service/services.py
@@ -0,0 +1,56 @@
+import datetime
+import boto3
+from botocore.exceptions import ClientError
+
+from settings import *
+
+#TODO: More error handling
+
+#Create Resource
+def connect():
+    dynamo_resource = boto3.resource(
+        'dynamodb',
+        aws_access_key_id=AWS_ACCESS_KEY_ID,
+        aws_secret_access_key=AWS_SECRET_ACCESS_KEY,
+        region_name=REGION_NAME
+        )
+    return dynamo_resource
+
+
+#UPDATE
+def update_item(athlete_id, date_time, location, country, dynamo):
+    table = dynamo.Table(ATHLETE_AVAILABILITY_TABLE)
+
+    date_time = datetime.datetime.fromisoformat(date_time)
+
+    date_attribute = date_time.date().isoformat()
+    time_attribute = date_time.time().isoformat()
+
+    try:
+        response = table.update_item(
+            Key={
+                'athlete_id': athlete_id,
+                'date': date_attribute,
+            },
+            UpdateExpression='set available_time = :t, location_address = :loc, location_country = :c',
+            ExpressionAttributeValues={
+                    ':t' : time_attribute,
+                    ':loc': location,
+                    ':c': country
+            },
+            ReturnValues="ALL_NEW"
+        )
+    except ClientError as e:
+        print("ERR:", e.response['Error']['Message'])
+    else:
+        return response
+
+
+#update availability
+def update_availability(athlete_id, date_time, location, country):
+    dynamo = connect()
+    resp = update_item(athlete_id, date_time, location, country, dynamo=dynamo)
+    if resp:
+        return UPDATE_SUCCESS
+    else:
+        return None
---
+++ Backend/Services/update-athlete-availability-service/settings.py
@@ -0,0 +1,14 @@
+from decouple import config
+
+ATHLETE_AVAILABILITY_TABLE= "AthleteAvailability"
+
+AWS_ACCESS_KEY_ID     = config("AWS_ACCESS_KEY_ID")
+AWS_SECRET_ACCESS_KEY = config("AWS_SECRET_ACCESS_KEY")
+REGION_NAME           = config("REGION_NAME")
+
+METHOD_NOT_FOUND = 'Method not recognised'
+ITEM_ALREADY_EXISTS = 'Initialisation could not be completed, item may already exist'
+ITEM_DOES_NOT_EXIST = 'Initialisation could not be completed, item may not exist'
+INIT_SUCCESS = 'Initialised Successfully'
+UPDATE_SUCCESS = 'Updated Successfully'
+INVALID_REQUEST_BODY = 'Invalid Request Body'
