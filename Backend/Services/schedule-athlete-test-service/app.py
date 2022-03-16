5bf7d675c41c454e26593d27afb7af492620a8b4
David Dunne <50323487+ddunne6@users.noreply.github.com>
2022-03-16 22:22:40 +0000

    moved service to new folder structure

--- Backend/Services/schedule-athlete-test-service/app.py
+++ Backend/Services/schedule-athlete-test-service/app.py
@@ -11,9 +11,9 @@ app = Flask(__name__)
 def assign_test():
     if request.is_json and request.method == 'POST':
         form = CreateTestForm(athlete_id=request.get_json().get('athlete_id'),
-                            date=request.get_json().get('date'),
-                            tester_id=request.get_json().get('tester_id'),
-                            orchestrator_id=request.get_json().get('orchestrator_id'))
+                              date=request.get_json().get('date'),
+                              tester_id=request.get_json().get('tester_id'),
+                              orchestrator_id=request.get_json().get('orchestrator_id'))
         if form.validate():
             test_item, dynamo_msg = create_test(athlete_id=form.athlete_id,
                                                 orchestrator_id=form.orchestrator_id,
@@ -26,22 +26,26 @@ def assign_test():
             else:
                 return jsonify({'message': ITEM_ADDED_SUCCESSFULLY, 'dynamodb_msg': dynamo_msg, 'test_item': test_item}), 201
     return jsonify({'form': {
-        'availability_id' : 'str',
-        'date' : 'str',
+        'availability_id': 'str',
+        'date': 'str',
         'tester_id': 'str',
-        'orchestrator_id' : 'str'
+        'orchestrator_id': 'str'
     }})
+
 
 @app.route("/upload-test-result", methods=['GET', 'POST'])
 def upload_test_result():
     if request.method == 'POST' and request.is_json:
-        form = UpdateTestResultForm(request.get_json().get('test_datetime'), request.get_json().get('tester_id'), request.get_json().get('test_result'))
+        form = UpdateTestResultForm(request.get_json().get('test_datetime'), request.get_json(
+        ).get('tester_id'), request.get_json().get('test_result'))
         if form.validate():
-            response, http_code = update_test_result(tester_id=form.tester_id, test_datetime=form.test_datetime, test_result=form.result)
+            response, http_code = update_test_result(
+                tester_id=form.tester_id, test_datetime=form.test_datetime, test_result=form.result)
             if http_code == 200:
                 return jsonify({'message': ITEM_UPDATED_SUCCESSFULLY, 'dynamodb_msg': response}), http_code
             else:
                 return jsonify({'message': ITEM_PARAMETERS_INVALID, 'dynamodb_msg': response}), http_code
+
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=5000)
