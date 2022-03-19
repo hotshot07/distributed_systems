a4288b2141ca5501b79f0eabd8d4f434847e4ad6
Mark Hanrahan <markh@Marks-MacBook-Pro.local>
2022-03-19 17:37:46 +0000

    Fixed form validation, added constraints on date availability

--- Backend/Services/update-athlete-availability-service/settings.py
+++ Backend/Services/update-athlete-availability-service/settings.py
@@ -12,3 +12,5 @@ ITEM_DOES_NOT_EXIST = 'Initialisation could not be completed, item may not exist
 INIT_SUCCESS = 'Initialised Successfully'
 UPDATE_SUCCESS = 'Updated Successfully'
 INVALID_REQUEST_BODY = 'Invalid Request Body'
+INVALID_TIME_RESOLUTION_GIVEN = 'The user has supplied an incorrect time format, the supplied utc time-slot must start on the hour. For example, 16:00 not 16:01'
+INVALID_TIME_FORMAT_GIVEN = 'The user has not given time in the correct format, please give utc time in iso (8601) format'
