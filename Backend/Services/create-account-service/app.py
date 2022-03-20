4f3b12f2ab1260bb086cd218522336fc5370dc77
hotshot07 <aroram@tcd.ie>
2022-03-20 00:02:16 +0000

    initialized stuff

---
+++ Backend/Services/create-account-service/app.py
@@ -0,0 +1,27 @@
+from flask import Flask, jsonify, request
+import requests
+
+
+app = Flask(__name__)
+
+
+@app.route("/create-athlete-account", methods=['GET', 'POST'])
+def create_athlete_account():
+    if request.method == 'POST':
+        data = request.get_json()
+        # url = "http://localhost:5000/create-athlete-account"
+        # response = requests.post(url, json=data)
+        # return jsonify(response.json())
+    else:
+        return jsonify({"error": "Method not allowed"})
+
+
+@app.route("/create-ado-account", methods=['GET', 'POST'])
+
+
+@app.route("/create-tester-account", methods=['GET', 'POST'])
+
+
+
+if __name__ == "__main__":
+    app.run(host="0.0.0.0", port=5000)
---
+++ Backend/Services/create-account-service/settings.py
@@ -0,0 +1,5 @@
+from decouple import config
+
+AWS_ACCESS_KEY_ID     = config("AWS_ACCESS_KEY_ID")
+AWS_SECRET_ACCESS_KEY = config("AWS_SECRET_ACCESS_KEY")
+REGION_NAME           = config("REGION_NAME")
