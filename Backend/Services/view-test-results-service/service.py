0fd5ada5bd977e5a43a884bf338dd46432dd595a
Saksham9399 <saagarwa@tcd.ie>
2022-03-24 23:38:47 +0000

    Validation for countries in view test results service

--- Backend/Services/view-test-results-service/service.py
+++ Backend/Services/view-test-results-service/service.py
@@ -1,6 +1,8 @@
+import boto3
 from boto3.dynamodb.conditions import Key
 from decouple import config
-import boto3
+
+from utils import country_list
 
 AWS_ACCESS_KEY_ID = config("AWS_ACCESS_KEY_ID")
 AWS_SECRET_ACCESS_KEY = config("AWS_SECRET_ACCESS_KEY")
@@ -13,12 +15,12 @@ def connect():
     :return: table of interest
     """
     dynamo_resource = boto3.resource(
-        'dynamodb',
+        "dynamodb",
         aws_access_key_id=AWS_ACCESS_KEY_ID,
         aws_secret_access_key=AWS_SECRET_ACCESS_KEY,
-        region_name=REGION_NAME
+        region_name=REGION_NAME,
     )
-    table = dynamo_resource.Table('AthleteTest')
+    table = dynamo_resource.Table("AthleteTest")
     return table
 
 
@@ -28,8 +30,15 @@ def get_test_results(country):
     :param country: country whose test results are asked
     :return: json response
     """
-    response = connect().query(
-        IndexName='country-index',
-        KeyConditionExpression=Key('country').eq(country)
-    )
-    return response if response['Items'] else f'No test results found for country {country}'
+    if country in country_list:
+
+        response = connect().query(
+            IndexName="country-index", KeyConditionExpression=Key("country").eq(country)
+        )
+        return (
+            response
+            if response["Items"]
+            else f"No test results found for country {country}"
+        )
+    else:
+        return f"Invalid country name {country}"
