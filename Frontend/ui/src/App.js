8e5ad8c82cf161e5269861103f54083ad7bafb68
Sean Doyle <doyles35@tcd.ie>
2022-03-24 06:34:28 +0000

    Tidied up navbar

--- Frontend/ui/src/App.js
+++ Frontend/ui/src/App.js
@@ -1,13 +1,20 @@
 import './SignIn.js';
 import React from 'react'
 import SignIn from './SignIn.js';
-import ResponsiveAppBar from './NavBar.js'
+import NavBar from './NavBar.js'
+import HowTo from './HowTo.js'
+import { BrowserRouter, Routes, Route } from "react-router-dom";
 
 function App() {
   return (
     <div className="App">
-      <ResponsiveAppBar />
-      <SignIn />
+      <BrowserRouter>
+      <NavBar />
+      <Routes>
+        <Route path="/" element={<SignIn />} />
+        <Route path="/howto" element={<HowTo />} />
+      </Routes>
+    </BrowserRouter>
     </div>
   );
 }
