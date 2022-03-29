b77afe0928d994fcae78afc68c8872e0079fc866
Ashutosh <ashutoshb2000@gmail.com>
2022-03-29 22:15:25 +0100

    settings file

--- Backend/Services/view-test-results-service/service.py
+++ Backend/Services/view-test-results-service/service.py
@@ -1,11 +1,6 @@
 from boto3.dynamodb.conditions import Key
-from decouple import config
 import boto3
-
-AWS_ACCESS_KEY_ID = config("AWS_ACCESS_KEY_ID")
-AWS_SECRET_ACCESS_KEY = config("AWS_SECRET_ACCESS_KEY")
-REGION_NAME = config("REGION_NAME")
-
+from .settings import ATHLETE_TEST_TABLE, AWS_ACCESS_KEY_ID, AWS_SECRET_ACCESS_KEY, REGION_NAME
 
 def connect():
     """
@@ -18,7 +13,7 @@ def connect():
         aws_secret_access_key=AWS_SECRET_ACCESS_KEY,
         region_name=REGION_NAME
     )
-    table = dynamo_resource.Table('AthleteTest')
+    table = dynamo_resource.Table(ATHLETE_TEST_TABLE)
     return table
 
 
---
+++ Backend/Services/view-test-results-service/settings.py
@@ -0,0 +1,8 @@
+import os
+
+AWS_ACCESS_KEY_ID = os.getenv("AWS_ACCESS_KEY_ID")
+AWS_SECRET_ACCESS_KEY = os.getenv("AWS_SECRET_ACCESS_KEY")
+REGION_NAME = os.getenv("REGION_NAME")
+
+
+ATHLETE_TEST_TABLE = "AthleteTest"
