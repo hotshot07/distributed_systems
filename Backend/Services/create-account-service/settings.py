622affcdded3f59c01c8b659f0f7c916298047bc
Mayank Arora <aroram@tcd.ie>
2022-03-29 12:24:54 +0100

    Actual Account creation (#20)
    * check if userid exists to create account
    
    * interesting stuff
    
    * done
    
    * fixed a bug
    
    * dont really need countries
    
    * autopep8 baby
    
    * updated comments
    
    * req changes

--- Backend/Services/create-account-service/settings.py
+++ Backend/Services/create-account-service/settings.py
@@ -1,8 +1,12 @@
 import os
 from decouple import config
 
-AWS_ACCESS_KEY_ID     = os.getenv("AWS_ACCESS_KEY_ID")
+AWS_ACCESS_KEY_ID = os.getenv("AWS_ACCESS_KEY_ID")
 AWS_SECRET_ACCESS_KEY = os.getenv("AWS_SECRET_ACCESS_KEY")
-REGION_NAME           = os.getenv("REGION_NAME")
+REGION_NAME = os.getenv("REGION_NAME")
 
-USER_PROFILE = config("USER_PROFILE")
+ADMIN_ALLOWED_ACCOUNTS = set(['Orchestrator', 'WADA'])
+
+USER_PROFILE = "UserProfile"
+AUTH_TABLE = "Auth"
+COUNTRY_ADO = "CountryAdo"
--- Backend/scripts/post_data_to_db.py
+++ Backend/scripts/post_data_to_db.py
@@ -2,67 +2,65 @@ from urllib import response
 from settings import *
 import boto3
 from generate_ids import create_country_ado_dict, generate_ten_digit_ids
-import time 
+import time
 
 resource = boto3.resource(
     'dynamodb',
-    aws_access_key_id     = AWS_ACCESS_KEY_ID,
-    aws_secret_access_key = AWS_SECRET_ACCESS_KEY,
-    region_name           = REGION_NAME,
+    aws_access_key_id=AWS_ACCESS_KEY_ID,
+    aws_secret_access_key=AWS_SECRET_ACCESS_KEY,
+    region_name=REGION_NAME,
 )
 
 
-CountryAdoId = resource.Table(COUNTRY_ADO_ID_TABLE)
+CountryAdo = resource.Table("CountryAdo")
 UserProfile = resource.Table(USER_PROFILE)
 
-if __name__ == "__main__":
-    
-    for country in create_country_ado_dict():
-        try:
-            response = CountryAdoId.put_item(
-                Item = country
-            )
-            time.sleep(1)
-        except Exception as e:
-            print(e.args)
-            
-        print(response)
-        
-        
-def auth_function(userid,pwd):
-    
-    # check if creds exist in dynamo 
-    
-    userid_pwd = {
-        'userid': userid,
-        'pwd': pwd
-    }
-    
-    # db has userid, pwd, hulib (has user logged in before)
-    
-    response = CountryAdoId.query(
-                Item = userid_pwd
-            )
-    
-    # if item exists, check if hulib is true
-    # if false, when returning return "inital login"
-    
-    # update hublib to true
-    
-    # return 'OK', 200
-     
-    
-    
+# if __name__ == "__main__":
+
+#     for country in create_country_ado_dict():
+#         try:
+#             response = CountryAdo.put_item(
+#                 Item = country
+#             )
+#             time.sleep(1)
+#         except Exception as e:
+#             print(e.args)
+
+#         print(response)
+
+
+# def auth_function(userid,pwd):
+
+#     # check if creds exist in dynamo
+
+#     userid_pwd = {
+#         'userid': userid,
+#         'pwd': pwd
+#     }
+
+#     # db has userid, pwd, hulib (has user logged in before)
+
+#     response = CountryAdoId.query(
+#                 Item = userid_pwd
+#             )
+
+# if item exists, check if hulib is true
+# if false, when returning return "inital login"
+
+# update hublib to true
+
+# return 'OK', 200
+
 
 # if __name__ == "__main__":
-    
+
 #     response = CountryAdoId.delete_item(
 #         Key = {
 #             'Country':'Ireland',
 #             'Id':2
 #         }
 #     )
-    
+
 #     print(response)
 
 # demo_countries = ['United States of America', 'United Kingdom' ,'Canada' , 'Ireland']
@@ -76,27 +74,11 @@ def auth_function(userid,pwd):
 #             'AccountType': 'WADA',
 #             'AccountStatus': 'Inactive'
 #         }
-        
+
 #         response = UserProfile.put_item(
 #                 Item = item_dict
 #             )
-        
+
 #         print(response)
-        
+
 #         time.sleep(1)
-
-        
-        
-        
-        
-        
-        
-        
-        
-                
-     
-
-
-
-
-
