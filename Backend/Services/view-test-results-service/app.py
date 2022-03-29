096799cdc3f5e687b4e3edaf0e6d2b78e4c502a2
Ashutosh <ashutoshb2000@gmail.com>
2022-03-29 22:21:31 +0100

    gunicorn added

--- Backend/Services/view-test-results-service/app.py
+++ Backend/Services/view-test-results-service/app.py
@@ -1,5 +1,6 @@
 from flask import Flask
 from service import get_test_results
+import logging
 app = Flask(__name__)
 
 
@@ -15,3 +16,10 @@ def view_results(country: str):
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=3000)
+
+if __name__ != "__main__":
+    # if we are not running directly, we set the loggers
+    gunicorn_logger = logging.getLogger("gunicorn.error")
+    app.logger.handlers = gunicorn_logger.handlers
+    app.logger.setLevel(gunicorn_logger.level)
+    app.logger.info("View test results service is now running!")
