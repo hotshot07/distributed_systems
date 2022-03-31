51383e7236bf5ef50f54a7d58f2f384fd3a62d81
David Dunne <50323487+ddunne6@users.noreply.github.com>
2022-03-31 17:01:56 +0100

    added validation to test service along with some logging (#30)

--- Backend/Services/schedule-athlete-test-service/dynamodb_handler.py
+++ Backend/Services/schedule-athlete-test-service/dynamodb_handler.py
@@ -1,5 +1,7 @@
+from urllib import response
 import boto3
 import botocore
+from boto3.dynamodb.conditions import Key
 from settings import *
 from models import *
 
@@ -12,23 +14,27 @@ resource = boto3.resource(
 
 client = boto3.client(
     'dynamodb',
-    aws_access_key_id     = AWS_ACCESS_KEY_ID,
-    aws_secret_access_key = AWS_SECRET_ACCESS_KEY,
-    region_name           = REGION_NAME,
+    aws_access_key_id=AWS_ACCESS_KEY_ID,
+    aws_secret_access_key=AWS_SECRET_ACCESS_KEY,
+    region_name=REGION_NAME,
 )
 
 AthleteTestTable = resource.Table(ATHELTE_TEST_TABLE)
 AthleteAvailabilityTable = resource.Table(ATHLETE_AVAILABILITY_TABLE)
-
+UserProfileTable = resource.Table(USER_PROFILE_TABLE)
 
 def create_test_using_transaction(athlete_id, date, tester_id, orchestrator_id):
+    import traceback
     try:
         athlete_test: AthleteTest = AthleteTest(
             athlete_id, date, tester_id, orchestrator_id)
+        validate_item = athlete_test.validate_item()
+        if not validate_item[0]:
+            return validate_item[1], 400
         athlete_test_json = athlete_test.to_dynamo_json()
     except Exception as e:
-        return e.args, ITEM_PARAMETERS_INVALID
-    # TODO ? Might need to use a form of optimistic locking
+        traceback.print_exc()
+        return COULD_NOT_CREATE_TEST_MODEL, 400
     try:
         response = client.transact_write_items(
             TransactItems=[
@@ -36,12 +42,12 @@ def create_test_using_transaction(athlete_id, date, tester_id, orchestrator_id):
                     'Update': {
                         'TableName': ATHLETE_AVAILABILITY_TABLE,
                         'Key': {
-                            'athlete_id': {'S':athlete_test.athlete.user_id},
-                            'date': {'S':athlete_test.start_date},
+                            'athlete_id': {'S': athlete_test.athlete.user_id},
+                            'date': {'S': athlete_test.start_date},
                         },
                         'UpdateExpression': 'SET assigned_test=:t',
                         'ExpressionAttributeValues': {
-                            ':t': {'BOOL':True},
+                            ':t': {'BOOL': True},
                         },
                         'ConditionExpression': 'attribute_not_exists(assigned_test)'
                     }
@@ -56,8 +62,9 @@ def create_test_using_transaction(athlete_id, date, tester_id, orchestrator_id):
             ]
         )
     except Exception as e:
-        return e.args, ITEM_PARAMETERS_INVALID
-    return athlete_test_json, response
+        traceback.print_exc()
+        return TRANSACTION_FAILED, 400
+    return response, 200
 
 def update_test_result(tester_id, test_datetime, test_result):
     try:
@@ -79,3 +86,8 @@ def update_test_result(tester_id, test_datetime, test_result):
     else:
         return response, 200
 
+def check_user_exists(user_id, user_role):
+    response = UserProfileTable.query(IndexName="Id-index", KeyConditionExpression=Key('Id').eq(user_id))
+    if response.get("Count", 0) == 1:
+        return response.get("Items")[0].get("AccountType") == user_role, response.get("Items")[0].get("Organization")
+    return False, None
--- Backend/Services/schedule-athlete-test-service/settings.py
+++ Backend/Services/schedule-athlete-test-service/settings.py
@@ -6,6 +6,8 @@ REGION_NAME = os.getenv("REGION_NAME")
 
 ATHLETE_AVAILABILITY_TABLE = 'AthleteAvailability'
 ATHELTE_TEST_TABLE = 'AthleteTest'
+USER_PROFILE_TABLE = 'UserProfile'
+COUNTRY_ADO_TABLE = 'CountryAdo'
 
 TEST_POSITIVE = "postive"
 TEST_NEGATIVE = "negative"
@@ -15,5 +17,16 @@ TEST_NOT_AVAILABLE = "not available"
 
 ITEM_COULD_NOT_BE_ADDED = "Item could not be added to database"
 ITEM_PARAMETERS_INVALID = "Item paramaters are invalid"
+COULD_NOT_CREATE_TEST_MODEL = "Failed to create test object from provided parameters"
+TRANSACTION_FAILED = "Transaction failed, check item parameters. Tester already assigned or availability slot has already been taken."
 ITEM_ADDED_SUCCESSFULLY = "Item added successfully!"
 ITEM_UPDATED_SUCCESSFULLY = "Item updated successfully!"
+
+FORM_TYPE_MUST_STRING = "Must be of type string (str)"
+COUNTRIES_DONT_MATCH = "Athlete and tester are not situated in the same country"
+
+# Roles
+ATHLETE_ROLE = "Athlete"
+TESTER_ROLE = "Tester"
+ORCHESTRATOR_ROLE = "Orchestrator"
+WADA_ROLE = "WADA"
