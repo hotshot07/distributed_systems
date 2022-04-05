5e2a11d31c2d116760c8b7c2dceeabf6a9a3288a
Ashutosh Bansal <56742106+Ashutosh2302@users.noreply.github.com>
2022-04-05 19:54:03 +0100

    Caching (#34)
    * caching
    
    * redis cache
    
    * flask default caching
    
    * flask default caching
    
    * formatting
    
    * added auth
    
    * auth
    
    * removed admin from auth service

--- Backend/Services/view-test-results-service/app.py
+++ Backend/Services/view-test-results-service/app.py
@@ -1,17 +1,26 @@
 from flask import Flask
 from flask_cors import CORS
 from service import get_test_results
+from auth import token_required, WADA, ORCHESTRATOR, ADMIN
 
 import logging
-
+from flask_caching import Cache
 from utils import country_list
 
 
 app = Flask(__name__)
+cache = Cache(
+    app,
+    config={
+        "CACHE_TYPE": "SimpleCache",
+    },
+)
 CORS(app)
 
 
 @app.route("/view-test-results/<string:country>", methods=["GET"])
+@token_required([WADA, ORCHESTRATOR, ADMIN])
+@cache.cached(timeout=50)
 def view_results(country: str):
     """
     Fetch test results
@@ -39,8 +48,8 @@ def view_results(country: str):
         return f"Invalid country name {country}"
 
 
-if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=3000)
+if __name__ == "__main__":
+    app.run(host="0.0.0.0", port=3000)
 
 if __name__ != "__main__":
     # if we are not running directly, we set the loggers
