23fd7dcf234404e5897e4c19ea47c33cd82538cd
Ashutosh Bansal <56742106+Ashutosh2302@users.noreply.github.com>
2022-04-11 20:17:53 +0100

    added pagination (#45)

--- Backend/Services/view-test-results-service/service.py
+++ Backend/Services/view-test-results-service/service.py
@@ -29,12 +29,36 @@ def get_test_results(country):
     :param country: country whose test results are asked
     :return: json response
     """
+    index = 1
+    data_to_return = {}
 
     response = connect().query(
         IndexName="country-index", KeyConditionExpression=Key("country").eq(country)
     )
+    queryCount = 1
+
+    items = response['Items']
+    for item in items:
+        data_to_return[index] = item
+        index += 1
+        
+    queryCount += 1
+
+    while 'LastEvaluatedKey' in response:
+        key = response['LastEvaluatedKey']
+        response = connect().query(
+        IndexName="country-index", KeyConditionExpression=Key("country").eq(country), ExclusiveStartKey=key
+    )
+        items = response['Items']
+        for item in items:
+            data_to_return[index] = item
+            index += 1
+        
+        queryCount += 1
+
     return (
-        response
-        if response["Items"]
+        data_to_return
+        if data_to_return
         else f"No test results found for country {country}"
     )
+    
