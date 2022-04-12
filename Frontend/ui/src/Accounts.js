fe45457d2e49a9a033e53bf61b9caa0c010be070
Sean Doyle <doyles35@tcd.ie>
2022-04-12 22:45:00 +0100

    Added accounting updating to Accounts page.

--- Frontend/ui/src/Accounts.js
+++ Frontend/ui/src/Accounts.js
@@ -1,5 +1,6 @@
 import React, { useContext, useRef } from 'react'
 import { TokenContext } from './Store'
+import axios from "axios";
 
 // mui imports
 import Divider from '@mui/material/Divider';
@@ -17,6 +18,10 @@ import Typography from '@mui/material/Typography';
 import { CssBaseline } from '@mui/material';
 
 
+const ENDPOINT = 'localhost';
+const PORT = '5000';
+
+
 function Accounts() {
     const [globalAuthData, setGlobalAuthData] = useContext(TokenContext)
     console.log("GOLBAL AUTH DATA")
@@ -28,42 +33,110 @@ function Accounts() {
     const acNumAccountsRef = useRef('');
     const [accountType, setAccountType] = React.useState('');
 
+    // *Account update refs.
+    const updateIdRef = useRef('');
+    const updateFirstNameRef = useRef('');
+    const updateLastNameRef = useRef('');
+    const updateNameOfAdoRef = useRef('');
+    const updateCountryRef = useRef('');
+    const updateEmailRef = useRef('');
+    const updatePhoneNumberRef = useRef('');
+    const [accountTypeUpdate, setAccountTypeUpdate] = React.useState('');
+
+    async function handleCreateTest() {
+        const auth_and_cors_headers = {
+            'crossDomain': 'true',
+            "X-Access-Token": `${globalAuthData.token}`
+        }
+
+        await axios.post(`http://${ENDPOINT}:${PORT}/create-n-accounts`,
+            {
+                Id: acIdRef.currrent.value,
+                Organization: acNameOfAdoRef.current.value,
+                NumberOfAccounts: acNumAccountsRef.current.value,
+                AccountType: accountType
+            }, { headers: auth_and_cors_headers }).then((response) => {
+                console.log(response);
+            }).catch((err) => {
+                console.log(err);
+            });
+    }
+
+    async function handleUpdateAccount() {
+        const auth_and_cors_headers = {
+            'crossDomain': 'true',
+            "X-Access-Token": `${globalAuthData.token}`
+        }
+
+        await axios.post(`http://${ENDPOINT}:${PORT}/update-${accountTypeUpdate.toLocaleLowerCase()}-account`,
+            {
+                Id: updateIdRef.current.value,
+                Organization: updateNameOfAdoRef.current.value,
+                FirstName: updateFirstNameRef.current.value,
+                LastName: updateLastNameRef.current.value,
+                Email: updateEmailRef.current.value,
+                Country: updateCountryRef.current.value,
+                PhoneNumber: updatePhoneNumberRef.current.value
+            }, { headers: auth_and_cors_headers }).then((response) => {
+                console.log(response);
+            }).catch((err) => {
+                console.log(err);
+            });
+    }
 
     return (
         <div>
             <Grid container sx={{ alignItems: 'center', }} justifyContent="center">
-                <Typography component="h1" variant="h2" sx={{m:5}}>Account Manager</Typography>
+                <Typography component="h1" variant="h2" sx={{ m: 5 }}>Account Manager</Typography>
             </Grid>
             <Grid container direction="column" alignItems="center" justifyContent="center"
             >
                 <CssBaseline />
 
                 <Stack direction="row" divider={<Divider orientation="vertical" flexItem />} spacing={10} sx={{ mt: 2 }}>
-                    {CreateAccountCard()}
-                    <Card>
-                        <Stack direction="column" divider={<Divider orientation="vertical" flexItem />} spacing={2} sx={{ m: 2 }} >
-                            <Box sx={{ alignItems: 'center', }}>
-                                <Typography component="h1" variant="h5">Update Account</Typography>
-                            </Box>
-
-                            <TextField id="outlined-basic" label="Other" variant="outlined" />
-                            <TextField id="outlined-basic" label="Other 2" variant="outlined" />
-                            <TextField id="outlined-basic" label="Other 3" variant="outlined" />
-                            <Button type="submit" fullWidth variant="contained" sx={{ mt: 3, mb: 2 }}>Do Something Else</Button>
-                        </Stack>
-                    </Card>
+                    {CreateNAccountsCard()}
+                    {UpdateAccountCard()}
                 </Stack>
             </Grid>
         </div >
     )
 
-    function CreateAccountCard() {
+    function UpdateAccountCard() {
+        return <Card>
+            <Stack direction="column" divider={<Divider orientation="vertical" flexItem />} spacing={2} sx={{ m: 2 }}>
+                {updateFormHeader()}
+                {updateForm()}
+                {DropDownUpdateAccountType()}
+                <Button type="submit" onClick={handleUpdateAccount} fullWidth variant="contained" sx={{ mt: 3, mb: 2 }}>Do Something Else</Button>
+            </Stack>
+        </Card>;
+    }
+
+    function updateFormHeader() {
+        return <Box sx={{ alignItems: 'center', }}>
+            <Typography component="h1" variant="h5">Update Account</Typography>
+        </Box>;
+    }
+
+    function updateForm() {
+        return <>
+            <TextField id="outlined-basic" label="ID" variant="outlined" inputRef={updateIdRef} />
+            <TextField id="outlined-basic" label="Email" variant="outlined" inputRef={updateEmailRef} />
+            <TextField id="outlined-basic" label="Organization Name" variant="outlined" inputRef={updateNameOfAdoRef} />
+            <TextField id="outlined-basic" label="First Name" variant="outlined" inputRef={updateFirstNameRef} />
+            <TextField id="outlined-basic" label="Last Name" variant="outlined" inputRef={updateLastNameRef} />
+            <TextField id="outlined-basic" label="Country" variant="outlined" inputRef={updateCountryRef} />
+            <TextField id="outlined-basic" label="Phone Number" variant="outlined" inputRef={updatePhoneNumberRef} />
+        </>;
+    }
+
+    function CreateNAccountsCard() {
         return <Card>
             <Stack direction="column" divider={<Divider orientation="vertical" flexItem />} spacing={2} sx={{ m: 2 }}>
                 {CreateAccountTitle()}
                 {CreateAccountTopForm()}
                 {DropDownAccountType()}
-                <Button type="submit" fullWidth onClick={() => { console.log(accountType); } } variant="contained" sx={{ mt: 3, mb: 2 }}> Create Account</Button>
+                <Button type="submit" fullWidth onClick={handleCreateTest} variant="contained" sx={{ mt: 3, mb: 2 }}> Create Account</Button>
             </Stack>
         </Card>;
     }
@@ -96,6 +169,27 @@ function Accounts() {
                     <MenuItem value={"Athlete"}>Athlete</MenuItem>
                     <MenuItem value={"Orchestrator"}>Orchestrator</MenuItem>
                     <MenuItem value={"Admin"}>Admin</MenuItem>
+                    <MenuItem value={"Admin"}>Tester</MenuItem>
+                </Select>
+            </FormControl>
+        </Box>;
+    }
+
+    function DropDownUpdateAccountType() {
+        return <Box sx={{ minWidth: 120 }}>
+            <FormControl fullWidth>
+                <InputLabel id="demo-simple-select-label">Account Type</InputLabel>
+                <Select
+                    labelId="demo-simple-select-label"
+                    id="demo-simple-select"
+                    value={accountTypeUpdate}
+                    label="Account Type"
+                    onChange={(event) => { setAccountTypeUpdate(event.target.value); }}
+                >
+                    <MenuItem value={"Athlete"}>Athlete</MenuItem>
+                    <MenuItem value={"Orchestrator"}>Orchestrator</MenuItem>
+                    <MenuItem value={"Admin"}>Admin</MenuItem>
+                    <MenuItem value={"Admin"}>Tester</MenuItem>
                 </Select>
             </FormControl>
         </Box>;
